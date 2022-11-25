7697c978235958ca664299230f594cd0df0ebc20
soham patil <mr.soham143@gmail.com>
2022-11-25 13:52:03 +0530

    all done

--- php/src/index.php
+++ php/src/index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />   -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
     <link rel="stylesheet" href="style.css">
     <title>Form </title>
 </head>
@@ -15,15 +18,15 @@
     <header>
         <h1>Create Profile</h1>
         <!-- <span>ID: 5988014</span> -->
-    </header>
+     </header>
     
-    <div class="ways">
+    <!-- <div class="ways">
         <ul>
         <li class="active">
             submit
         </li>
         </ul>
-    </div>
+    </div> -->
     
     <div class="sections">
         
@@ -34,14 +37,14 @@
             <input type="text" placeholder="Enter Your Name" id="name" name="name"/>
             <input type="email" name="email" id="email" placeholder="Enter Your Email">
             <div class="images">
-                <input type="file" name="image" id="image">
+                <input type="file" name="img" id="img">
             </div>
 
             <footer>
                 <ul>
-                <!-- <li><span id="reset" action="test.php">Show Data</span></li> -->
+                <!-- <li><span id="reset" aSELECT * FROM `formRespons` WHERE 1ction="test.php">Show Data</span></li> -->
                 <li>
-                    <input type="submit" value="Submit">
+                    <input type="submit" value="Submit" id="insert">
                 </li>
                 </ul>
             </footer> 
@@ -70,13 +73,39 @@
     </div>
     
     
-    </div>
-    <div class="notification"></div>
-    <footer>
+</div>
+<div class="notification"></div>
+<footer>
     <p>Copyright 2001 &copy; : All Rights Reserverd</p>
-    </footer>
+</footer>
 </body>
-</html>
+
+</html> 
+
+<script>  
+ $(document).ready(function(){  
+      $('#insert').click(function(){  
+           var image_name = $('#img').val();  
+           if(image_name == '')  
+           {  
+                alert("Please Select Image");  
+                return false;  
+           }  
+           else  
+           {  
+                var extension = $('#img').val().split('.').pop().toLowerCase();  
+                if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)  
+                {  
+                     alert('Invalid Image File');  
+                     $('#img').val('');  
+                     return false;  
+                }  
+           }  
+      });  
+ });  
+ </script>
+
+
 
 
 <?php
@@ -84,36 +113,45 @@
     $host = 'db';
 
     // Database use name
-    $user = 'MYSQL_USER';
+    $user = 'root';
     
     //database user password
-    $pass = 'MYSQL_PASSWORD';
+    $pass = 'MYSQL_ROOT_PASSWORD';
     
     // check the MySQL connection status
-    $conn = new mysqli($host, $user, $pass);
+    $conn = new mysqli($host, $user, $pass, 'imgUpload');
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }else {
         echo "Connection Successful !";
     }
-    // Taking all 5 values from the form data(input)
+
+    // Taking all 3 values from the form data(input)
     $name =  $_REQUEST['name'];
     $email = $_REQUEST['email'];
-    $img = $_REQUEST['img'];
-    
-    // echo "$name \n $email \n $img";
+    // $img = $_REQUEST['img'];
+    $image = $_FILES['img'] ;
+    // $image = addslashes(file_get_contents($_FILES['image']['tmp_name'])) ;
+    // $img = file_get_contents($image);
+
+    // $data = base64_encode($img);
+    echo "$name \n $email \n $img";
     // Performing insert query execution
     // here our table name is college
     // $sql = "INSERT INTO formResponse  VALUES ('$name','$email', '$img')";
-    $sql = "INSERT INTO formRespons  VALUES ('$name', '$email' , '$img')";
-    
-    if(mysqli_query($conn, $sql)){
-        echo "<h3>data stored in a database successfully."
+
+    $sql = "select imgUpload";
+    $sql1 = "INSERT INTO formRespons  VALUES ('$name', '$email' , '$image')";
+    echo mysqli_query($conn, $sql);
+
+    if(mysqli_query($conn, $sql1)){
+        echo "<h3>Data stored in a database successfully."
             . " Please browse your localhost php my admin"
             . " to view the updated data</h3>";
 
         echo nl2br("\n$name\n  "
-            . "\n $email \n $img ");
+            . "\n $email \n $image ");
     } else{
         echo "ERROR: Hush! Sorry $sql. "
             . mysqli_error($conn);
@@ -121,6 +159,4 @@
     
     // Close connection
     mysqli_close($conn);
-
-
 ?>
--- php/src/test.php
+++ php/src/test.php
@@ -1,14 +1,16 @@
+
+
 <?php 
 $host = 'db';
 
 // Database use name
-    $user = 'MYSQL_USER';
+    $user = 'root';
 
 //database user password
-    $pass = 'MYSQL_PASSWORD';
+    $pass = 'MYSQL_ROOT_PASSWORD';
 
 // check the MySQL connection status
-    $conn = new mysqli($host, $user, $pass);
+    $conn = new mysqli($host, $user, $pass, 'imgUpload');
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
@@ -24,9 +26,41 @@ $host = 'db';
         }
     }
 
-    foreach ($users as $user) {
-        echo "<br>";
-        echo $user->name . "    " . $user->email . "    " . $user->img;
-        echo "<br>";
-    }
+    // foreach ($users as $user1) {
+    //     echo "<br>";
+    //     echo $user1->name . "    " . $user1->email . "    " . $user1->img;
+    //     echo "<br>";
+    // }
 ?>
+
+
+<html>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+ 
+    <body>
+    <table class="table table-bordered">  
+                     <tr>  
+                          <th>Image</th>  
+                     </tr>  
+                <?php  
+                $query = "SELECT * FROM formRespons";  
+                $result = mysqli_query($conn, $query);  
+                $row = mysqli_fetch_array($result);  
+                while($row)
+                {  
+                     echo '  
+                          <tr>  
+                               <td>  
+                                    
+                                    <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="200" width="200" class="img-thumnail" />  
+                               </td>  
+                          </tr>  
+                     ';  
+                }  
+                ?>  
+    </table>
+    </body>
+
+</html>
